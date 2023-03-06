368a71ae9590047de0b1c42a17ccc92896a5d3f9
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-03-05 22:39:41 -0500

    asdf

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function generateRandomInteger(min, max) {
+function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function dynamicUrl() {
-  var url = "/img/" + generateRandomInteger(1, 21) + ".jpg";
+  var url = "/img/" + randomNum(1, 21) + ".jpg";
   console.log(url);
   var img = document.getElementById("imageid");
   img.src = url;
