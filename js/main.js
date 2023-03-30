781aebcbf1eac7b27738edffe3d929e400c63b9c
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-03-30 11:38:10 -0400

    asdf

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+let history = [];
+
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getImage() {
-  var poo = "/img/" + randomNum(1, 21) + ".jpg";
+  const randomNumber = randomNum(1, 21);
+  var poo = "/img/" + randomNumber + ".jpg";
   console.log(poo);
   var pee = document.getElementById("imageid");
   pee.src = poo;
+  history.push(randomNumber);
+  console.log(history);
+  document.getElementById("history").innerText += ` ${randomNumber}`;
 }
